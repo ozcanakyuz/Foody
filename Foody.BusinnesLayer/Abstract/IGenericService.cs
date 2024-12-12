3ac5e98e190a4c2e0b6d30cee9518b88a081ad4f
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-12 18:26:28 +0300

    Service Interfaces For Business Layer

---
+++ Foody.BusinnesLayer/Abstract/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.BusinnesLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        void TInsert(T entity);
+        void TUpdate(T entity);
+        void DTelete(int id);
+        List<T> TGetAll();
+        T TGetById(int id);
+    }
+}
