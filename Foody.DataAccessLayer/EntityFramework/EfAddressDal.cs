98f02557aa86541b36fae138f093a76061328496
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-15 11:11:35 +0300

    Admin Layout Page

--- Foody.DataAccessLayer/EntityFramework/EfAddressDal.cs
+++ Foody.DataAccessLayer/EntityFramework/EfAddressDal.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Foody.DataAccessLayer.EntityFramework
 {
-    public class EfAddressDal : GenericRepository<Address>, IAdressDal
+    public class EfAddressDal : GenericRepository<Address>, IAddressDal
     {
         public EfAddressDal(FoodyContext context) : base(context)
         {
