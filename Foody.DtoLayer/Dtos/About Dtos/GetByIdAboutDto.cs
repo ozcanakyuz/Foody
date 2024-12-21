342843183d2de2ee53bd0f6f574c45117c1b4e4e
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-21 12:14:17 +0300

    DtoLayer Classes

---
+++ Foody.DtoLayer/Dtos/About Dtos/GetByIdAboutDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DtoLayer.Dtos.About_Dtos
+{
+    public class GetByIdAboutDto
+    {
+        public int AboutId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
