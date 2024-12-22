15e4f090820b0f6c33159da02d431c4fc4711621
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-22 21:54:17 +0300

    Admin About Operations

---
+++ Foody.PresentationLayer/Controllers/AboutsController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Foody.BusinnesLayer.Abstract;
+using Foody.DtoLayer.Dtos.About_Dtos;
+using Foody.DtoLayer.Dtos.SliderDtos;
+using Foody.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.Controllers
+{
+    public class AboutsController : Controller
+    {
+        private readonly IAboutService _aboutService;
+        private readonly IMapper _mapper;
+        public AboutsController(IAboutService aboutService, IMapper mapper)
+        {
+            _aboutService = aboutService;
+            _mapper = mapper;
+        }
+
+        public IActionResult AboutList()
+        {
+            var values = _aboutService.TGetAll();
+            return View(_mapper.Map<List<ResultAboutDto>>(values));
+        }
+        [HttpGet]
+        public IActionResult CreateAbout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateAbout(CreateAboutDto createAboutDto)
+        {
+            var value = _mapper.Map<About>(createAboutDto);
+            _aboutService.TInsert(value);
+            return RedirectToAction("AboutList");
+        }
+        public IActionResult DeleteAbout(int id)
+        {
+            _aboutService.TDelete(id);
+            return RedirectToAction("AboutList");
+        }
+        [HttpGet]
+        public IActionResult UpdateAbout(int id)
+        {
+            var value = _aboutService.TGetById(id);
+            return View(_mapper.Map<GetByIdAboutDto>(value));
+        }
+        [HttpPost]
+        public IActionResult UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            var value = _mapper.Map<About>(updateAboutDto);
+            _aboutService.TUpdate(value);
+            return RedirectToAction("AboutList");
+        }
+    }
+}
