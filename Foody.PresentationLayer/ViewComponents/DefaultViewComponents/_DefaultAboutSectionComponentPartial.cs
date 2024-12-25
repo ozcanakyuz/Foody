1f9ce342bdc298a64134a086fa50edb2d54ab10a
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-25 14:16:56 +0300

    About Section In User Interface

---
+++ Foody.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultAboutSectionComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Foody.BusinnesLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultAboutSectionComponentPartial : ViewComponent
+    {
+        private readonly IAboutService _aboutService;
+        public _DefaultAboutSectionComponentPartial(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _aboutService.TGetAll();
+            return View(values);
+        }
+    }
+}
